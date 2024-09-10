1e6d6bf2d724c4296525354e5ed8b6ee3e2b331f
Hassan Jamil <h.jamil1713@gmail.com>
2024-09-10 17:40:23 +0500

    delete employee

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -77,5 +77,23 @@ namespace EmployeeAdminPortal.Controllers
 
             return Ok(employee);
         }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Employees.Remove(employee);
+
+            dbContext.SaveChanges();
+
+            return Ok("Employee deleted");
+        }
     }
 }

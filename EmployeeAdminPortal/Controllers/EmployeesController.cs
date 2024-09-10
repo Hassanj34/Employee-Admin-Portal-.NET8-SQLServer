f013f00d18e8a92bab338552d95b2ddcfce55bd8
Hassan Jamil <h.jamil1713@gmail.com>
2024-09-10 17:22:04 +0500

    get employees & add employee

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeAdminPortal.Controllers
@@ -7,9 +10,37 @@ namespace EmployeeAdminPortal.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-        public EmployeesController()
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
         {
-            
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Employees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeDTO employeeDTO) 
+        {
+            var employee = new Employee()
+            {
+                Name = employeeDTO.Name,
+                Email = employeeDTO.Email,
+                Phone = employeeDTO.Phone,
+                Salary = employeeDTO.Salary,
+            };
+
+            dbContext.Employees.Add(employee);
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
         }
     }
 }
